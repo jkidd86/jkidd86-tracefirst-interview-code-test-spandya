6c8b7bf918a3834c8dfa0e8f6535a73266024fc8
Sagar Pandya <sagarp@stinkyink.com>
2026-03-08 22:34:31 +0000

    #2 Add basic phone number validation

--- app/models/veterinarian.rb
+++ app/models/veterinarian.rb
@@ -2,4 +2,6 @@ class Veterinarian < ApplicationRecord
   has_many :tests, dependent: :restrict_with_exception
 
   acts_as_paranoid
+
+  validates :number, format: { with: /\A\+[1-9]\d{7,14}\z/, message: "must be international number format" }
 end
